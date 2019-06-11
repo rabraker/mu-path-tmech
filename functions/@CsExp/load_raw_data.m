c93ce3085c9c19fcba60910dbbb1eed09e092360
arnold (zeus) <abraker@fastmail.com>
2019-06-11 14:41:37 -0600

    Must create t after x, otherwise it will be empty.

--- functions/@CsExp/load_raw_data.m
+++ functions/@CsExp/load_raw_data.m
@@ -84,13 +84,13 @@ function self = load_raw_data(self, cs_paths, opts)
   self.met_ind(self.met_ind > 0) = -4;      
   self.idx_state_s = CsExp.divide_by_state(self.met_ind);
   
-  self.t = (0:length(self.x)-1)'*AFM.Ts;
+
   self.x = dat_meas(:, channel_map.x);
   self.x = self.x; %- min(self.x); % move to positive orthant.
   self.y = dat_meas(:, channel_map.y);
   self.y = self.y; %- min(self.y); % move to positive orthant.
-  
   self.ze = dat_meas(:, channel_map.ze);
+  self.t = (0:length(self.x)-1)'*AFM.Ts;
   
   if have_friction
       self.z_friction = dat_meas(:, channel_map.friction);
